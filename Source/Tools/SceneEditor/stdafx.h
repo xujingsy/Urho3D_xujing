04001d0cd042c1bf3b35975a1e584e33304cb6d7
xujingsy <xujingsy@163.com>
2014-07-16 14:51:59 +0800

    整理工程

--- Source/Tools/SceneEditor/stdafx.h
+++ Source/Tools/SceneEditor/stdafx.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "targetver.h"
+#ifndef _WIN32_WINNT            // 指定要求的最低平台是 Windows Vista
+#define _WIN32_WINNT 0x0600     // 将此值更改为相应的值，以适用于 Windows 的其他版本。
+#endif
 
 #include <stdio.h>
 #include <tchar.h>
